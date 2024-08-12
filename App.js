e685fc8597548078d3ef6015fc60abfe1b16aa2a
chukwutem-emi <chukwutememi@gmail.com>
2024-08-12 06:09:28 +0100

    lesson2

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, [React.createElement("h1", {},"This is Namaste React 🚀"), React.createElement("h2", {},"i'm an h2 tag"),])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
